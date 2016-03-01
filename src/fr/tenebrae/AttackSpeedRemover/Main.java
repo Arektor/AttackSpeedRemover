e5628fcc931e4d90c5ed7d3ba35155f74b46d795
Arektor <maxence@symfonye.com>
2016-03-01 19:08:30 +0100

    Add old damage restoration

--- src/fr/tenebrae/AttackSpeedRemover/Main.java
+++ src/fr/tenebrae/AttackSpeedRemover/Main.java
@@ -1,8 +1,11 @@
 package fr.tenebrae.AttackSpeedRemover;
 
+import java.util.ArrayList;
+
 import org.bukkit.World;
 import org.bukkit.attribute.Attributable;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,13 +13,16 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin implements Listener {
 	
 	public FileConfiguration config;
-	//16 would be enough, but we set it to 64 in case of some plugins would use custom attack speed values.
+	//16 would be enough, but we set it to 64 to prevent any issue with plugins using custom attack speed
 	public AttributeModifier modifier = new AttributeModifier("AttackSpeedRemover", 64.0D, Operation.ADD_NUMBER);
 	
 	@Override
@@ -38,16 +44,81 @@ public class Main extends JavaPlugin implements Listener {
 		return att;
 	}
 	
+	public Attributable restoreOldDamage(Attributable att, ItemStack item) {
+		String type = "Hand";
+		if (item != null) type = item.getType().toString();
+		if (type.contains("AXE")) {
+			if (type.contains("DIAMOND")) {
+				att = this.setAttackDamage(att, 6);
+			} else if (type.contains("GOLD") || type.contains("WOOD")) {
+				att = this.setAttackDamage(att, 3);
+			} else if (type.contains("STONE")) {
+				att = this.setAttackDamage(att, 4);
+			} else if (type.contains("IRON")) {
+				att = this.setAttackDamage(att, 5);
+			}
+		} else if (type.contains("SWORD")) {
+			if (type.contains("DIAMOND")) {
+				att = this.setAttackDamage(att, 7);
+			} else if (type.contains("GOLD") || type.contains("WOOD")) {
+				att = this.setAttackDamage(att, 4);
+			} else if (type.contains("STONE")) {
+				att = this.setAttackDamage(att, 5);
+			} else if (type.contains("IRON")) {
+				att = this.setAttackDamage(att, 6);
+			}
+		} else if (type.contains("SPADE")) {
+			if (type.contains("DIAMOND")) {
+				att = this.setAttackDamage(att, 4);
+			} else if (type.contains("GOLD") || type.contains("WOOD")) {
+				att = this.setAttackDamage(att, 1);
+			} else if (type.contains("STONE")) {
+				att = this.setAttackDamage(att, 2);
+			} else if (type.contains("IRON")) {
+				att = this.setAttackDamage(att, 3);
+			}
+		} else if (type.contains("PICKAXE")) {
+			if (type.contains("DIAMOND")) {
+				att = this.setAttackDamage(att, 5);
+			} else if (type.contains("GOLD") || type.contains("WOOD")) {
+				att = this.setAttackDamage(att, 2);
+			} else if (type.contains("STONE")) {
+				att = this.setAttackDamage(att, 3);
+			} else if (type.contains("IRON")) {
+				att = this.setAttackDamage(att, 4);
+			}
+		} else if (type.contains("HOE")) {
+			if (type.contains("DIAMOND")) {
+				att = this.setAttackDamage(att, 1);
+			} else if (type.contains("GOLD") || type.contains("WOOD")) {
+				att = this.setAttackDamage(att, 1);
+			} else if (type.contains("STONE")) {
+				att = this.setAttackDamage(att, 1);
+			} else if (type.contains("IRON")) {
+				att = this.setAttackDamage(att, 1);
+			}
+		} else {
+			att = this.setAttackDamage(att, 1);
+		}
+		return att;
+	}
+	
+	public Attributable setAttackDamage(Attributable att, double newAtkDmg) {
+		AttributeInstance instance = att.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+		instance.setBaseValue(newAtkDmg);
+		
+		for (AttributeModifier modifier : new ArrayList<AttributeModifier>(instance.getModifiers()))
+			if (!modifier.getName().contains("potion")) instance.removeModifier(modifier);
+		
+		return att;
+	}
+	
 	public boolean checkCondition(World world) {
 		if (config.getInt("whitelist_type") == 0) {
-			if (config.getStringList("whitelist").contains(world.getName())) {
-				return false;
-			}
+			if (config.getStringList("whitelist").contains(world.getName())) return false;
 			else return true;
 		} else {
-			if (config.getStringList("whitelist").contains(world.getName())) {
-				return true;
-			}
+			if (config.getStringList("whitelist").contains(world.getName())) return true;
 			else return false;
 		}
 	}
@@ -73,5 +144,21 @@ public class Main extends JavaPlugin implements Listener {
 		}
 		if (!checkCondition(evt.getPlayer())) return;
 		this.removeAttackSpeed(evt.getPlayer());
+	}
+	
+	@EventHandler
+	public void itemHeld(final PlayerItemHeldEvent evt) {
+		if (!checkCondition(evt.getPlayer())) return;
+		if (!checkCondition(evt.getPlayer().getWorld())) return;
+		new BukkitRunnable() { 
+			
+			@Override
+			public void run() {
+				restoreOldDamage(evt.getPlayer(), evt.getPlayer().getInventory().getItemInMainHand());
+			}
+			
+		}.runTaskLaterAsynchronously(this, 2L);
+		// Delaying the task with 2 ticks to avoid getting the old held item.
+		// Running it asynchronously to avoid general server lags with weird people keeping switching held item.
 	}
 }
